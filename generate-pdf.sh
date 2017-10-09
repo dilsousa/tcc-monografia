ecf1bd2b0eed6fdb51c4e87616d7a4ed52c1f1fa
André Formento <andreformento.sc@gmail.com>
2017-10-08 22:00:55 -0300

    Update sh

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-rm -rf latex-docker
 rm -rf monografia.*
 
-git clone https://github.com/blang/latex-docker
-
-latex-docker/dockercmd.sh /bin/sh -c "pdflatex tex/monografia.tex"
+./dockercmd.sh /bin/sh -c "pdflatex tex/monografia.tex"
