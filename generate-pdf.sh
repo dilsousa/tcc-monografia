2f30e3476eacb902367183cc921ff6b6126cb03e
André Formento <andreformento.sc@gmail.com>
2017-10-14 19:06:41 -0300

    Update generator

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -6,6 +6,11 @@ rm -rf abntex2-modelo-include-comandos.*
 
 ./dockercmd.sh /bin/sh -c "pdflatex tex/monografia.tex"
 
+# Compilers:
+# pdflatex
+# xelatex
+# lualatex
+
 mv monografia.pdf _monografia.pdf
 rm rm -rf monografia.*
 rm -rf abntex2-modelo-include-comandos.*
