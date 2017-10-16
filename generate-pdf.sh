2c53d9c14bbe96fd0690dbb6a9c53cb0c3021e62
André Formento <andreformento.sc@gmail.com>
2017-10-15 23:14:20 -0200

    Fix with duplicated generation

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
-docker build -t pdflatex . 
-docker run -it -v "$PWD":/data pdflatex
+cd tex
+rm -f monografia.pdf
+
+docker run -it -v "$PWD":/data -w /data andreformento/latex pdflatex monografia.tex
+
+rm -f monografia.pdf
+
+docker run -it -v "$PWD":/data -w /data andreformento/latex pdflatex monografia.tex
+
+rm -f *.fdb_latexmk
+rm -f *.aux
+rm -f *.fls
+rm -f *.idx
+rm -f *.ilg
+rm -f *.ind
+rm -f *.lof
+rm -f *.log
+rm -f *.lot
+rm -f *.toc
--- tex/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo aaa223
-pdflatex /monografia.tex
-
-cp /monografia.pdf /data/
