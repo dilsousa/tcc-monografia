153c9c8fc878316083b76c37e659ebc8972477ad
André Formento <andreformento.sc@gmail.com>
2017-10-20 22:03:52 -0200

    Add clear script

---
+++ clear.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd tex
+
+rm -f *.fdb_latexmk
+rm -f *.aux
+rm -f *.fls
+rm -f *.idx
+rm -f *.ilg
+rm -f *.ind
+rm -f *.lof
+rm -f *.log
+rm -f *.lot
+rm -f *.toc
+rm -f *.blg
+rm -f *.brf
+rm -f *.bbl
--- generate-pdf.sh
+++ generate-pdf.sh
@@ -10,18 +10,7 @@ rm -f monografia.pdf
 
 docker run -it -v "$PWD":/data -w /data andreformento/latex pdflatex monografia.tex
 
-rm -f *.fdb_latexmk
-rm -f *.aux
-rm -f *.fls
-rm -f *.idx
-rm -f *.ilg
-rm -f *.ind
-rm -f *.lof
-rm -f *.log
-rm -f *.lot
-rm -f *.toc
-rm -f *.blg
-rm -f *.brf
-rm -f *.bbl
-
 mv monografia.pdf ../
+
+cd ..
+./clear.sh
